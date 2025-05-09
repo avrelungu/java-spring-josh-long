f5ec35809749c4410666ce85993049dca5f9fb67
Aurel Lungu <aurel.lungu.dev@gmail.com>
2025-05-09 21:38:11 +0300

    sdkman and small customers implementation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into customer(name) values ('Aurel');
+insert into customer(name) values ('Tom');
+insert into customer(name) values ('Ben');
+insert into customer(name) values ('Luke');
+insert into customer(name) values ('Sam');
